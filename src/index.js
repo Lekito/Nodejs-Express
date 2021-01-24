bc0f6b23eda61902568e525a0e73b41d6afe0f0b
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-01-23 22:27:52 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,37 @@ const express = require('express'); // importamos
 const app = express(); // instaciamos/declaramos 
 
 app.get('/projects', (requeste, response) => {
-    return response.json({
-        message: 'Hello Dev!'
-    });
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'projeto 4'
+    ]);
+});
+
+app.put('/projects', (request, response) => {
+    return response.json([
+        'Projeto 5',
+        'Projeto 2',
+        'Projeto 3',
+        'projeto 4'
+    ]);
+});
+
+app.delete('/projects', (request, response) => {
+    return response.json([
+        'Projeto 5',
+        'Projeto 2',
+        'projeto 4'
+    ]);
 });
 
 app.listen(3333, () => {
